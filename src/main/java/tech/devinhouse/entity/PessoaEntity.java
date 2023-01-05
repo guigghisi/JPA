1daa532b1c0a35b774395fc7e4c93a9789d369bc
Guigghisi <guilhermeghisi14@gmail.com>
2023-01-05 15:42:36 -0300

    M2S09E4

--- src/main/java/tech/devinhouse/entity/PessoaEntity.java
+++ src/main/java/tech/devinhouse/entity/PessoaEntity.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.lang.NonNull;
 
 
 @Entity(name = "pessoas")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PessoaEntity {
 
     @Id
